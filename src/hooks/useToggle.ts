994e7ad56b17108b8166e14411f2d77068402942
pocj8ur4in <pocj8ur4in@gmail.com>
2024-02-29 00:15:14 +0900

    fix: @typescript-eslint/no-inferrable-types

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,7 +1,7 @@
 import { useReducer } from 'react';
 
 // useToggle 커스텀 훅 정의
-export const useToggle = (defaultFlag: boolean = false) => {
+export const useToggle = (defaultFlag = false) => {
   const [flag, makeToggle] = useReducer((previous) => !previous, defaultFlag); // useReducer로 상태와 토글 함수 생성
 
   return [flag, makeToggle] as const; // 토글된 상태와 토글 함수를 튜플로 반환
