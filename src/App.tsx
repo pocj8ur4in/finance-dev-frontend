c85f7ace0cbb9d84ce8f89716ae60e15ca2f60ab
pocj8ur4in <pocj8ur4in@gmail.com>
2024-02-29 03:10:07 +0900

    feat: 라우팅 및 어플리케이션 구조 설정
    # 동일한 화면에서 다른 경로로 라우팅될 수 있도록 App.tsx를 수정합니다.
    # 해당되는 각각의 경로에 대한 파일들을 생성합니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { Nav } from './Nav';
+import { Route, Routes } from 'react-router-dom';
+import { Login } from './components/Login';
+import { UserLayout } from './components/UserLayout';
+import { Users } from './components/Users';
+import { UserDetail } from './components/UserDetail';
+import { AlbumLayout } from './components/AlbumLayout';
+import { Albums } from './components/Albums';
+import { AlbumDetail } from './components/AlbumDetail';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Login />}></Route>
+        <Route path='/users/*' element={<UserLayout />}>
+          <Route index element={<Users />} />
+          <Route path=':userId/*' element={<UserDetail />} />
+          <Route path=':userId/albums/*' element={<AlbumLayout />}>
+            <Route index element={<Albums />} />
+            <Route path=':userId/albums/:albumId' element={<AlbumDetail />} />
+          </Route>
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
