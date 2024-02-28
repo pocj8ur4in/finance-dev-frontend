fa94c7916a4a420a35bb16884fd35152dd734149
pocj8ur4in <pocj8ur4in@gmail.com>
2024-02-29 02:27:23 +0900

    feat: nav 추가
    # 모든 화면에서 볼 수 있는 네비게이션 바에 대한 리액트 컴포넌트 Nav를 생성합니다.

---
+++ src/Nav.tsx
@@ -0,0 +1,7 @@
+export const Nav = () => (
+  <nav className='bg-blue-400 p-3 rounded-lg mb-4'>
+    <ul className='flex justify-between items-center text-white'>
+      <li className='mr-auto'>Hanaro Album</li>
+    </ul>
+  </nav>
+);
