62746ffc82ebdcf0f5ba9c13590b11d691d35671
pocj8ur4in <pocj8ur4in@gmail.com>
2024-02-29 00:16:34 +0900

    fix: @typescript-eslint/no-non-null-assertion

--- src/Index.tsx
+++ src/Index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </React.StrictMode>
+  );
+}
